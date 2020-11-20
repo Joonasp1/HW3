134959b7f21eb4a5303df22839ba972480a5da9a
Joonas Püks <joonaspyks@gmail.com>
2020-11-20 23:16:19 +0200

    Initial Vue integration

---
+++ res/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div id="app">
+    <login/>
+  </div>
+
+</template>
+
+
+
+<script>
+import login from "../../HW3_Vue/hw3/src/components/login.vue"
+
+export default {
+  name: "App",
+  components: {
+    login
+  }
+};
+
+</script>
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div id="app">
+    <login/>
+  </div>
+
+</template>
+
+
+
+<script>
+import login from "./components/login.vue"
+
+export default {
+  name: "App",
+  components: {
+    login
+  }
+};
+
+</script>
+
